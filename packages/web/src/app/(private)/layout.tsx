22a15398da464d5b3ca9a08e9b49b8cf8f267995
erivaldocazinga22 <cazarthdesogner@gmail.com>
2025-08-19 19:06:44 +0100

    refactor: clean up component code and improve formatting for consistency

--- packages/web/src/app/(private)/layout.tsx
+++ packages/web/src/app/(private)/layout.tsx
@@ -1,14 +1,30 @@
 import Link from "next/link";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <div>
-            <ul className="flex items-center gap-4 mb-4">
-                <li><Link href="/" className="hover:underline">Home</Link></li>
-                <li><Link href="/costs" className="hover:underline">Gastos</Link></li>
-                <li><Link href="/statistic" className="hover:underline">Estatistica</Link></li>
-            </ul>
-            {children}
-        </div>
-    )
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<div>
+			<ul className="flex items-center gap-4 mb-4">
+				<li>
+					<Link href="/" className="hover:underline">
+						Home
+					</Link>
+				</li>
+				<li>
+					<Link href="/costs" className="hover:underline">
+						Gastos
+					</Link>
+				</li>
+				<li>
+					<Link href="/statistic" className="hover:underline">
+						Estatistica
+					</Link>
+				</li>
+			</ul>
+			{children}
+		</div>
+	);
 }
