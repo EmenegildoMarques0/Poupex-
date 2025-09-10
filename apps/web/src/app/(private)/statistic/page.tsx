af50377b0f0804c6ac3d741b09c58720bba8dda1
Erivaldo Caginga <136964420+erivaldocazinga22@users.noreply.github.com>
2025-09-10 04:45:48 +0100

    fix(statistic): adjust chart component classes for consistent minimum height

--- apps/web/src/app/(private)/statistic/page.tsx
+++ apps/web/src/app/(private)/statistic/page.tsx
@@ -54,7 +54,7 @@ export default async function StatisticPage() {
                     labels: data.data.charts.monthlyChart.labels ?? [],
 					data: data.data.charts.monthlyChart.data ?? []
                 }}
-                className="md:col-span-2 w-full h-[400px]"
+                className="col-span-2 md:col-span-2 w-full min-h-[400px]"
             />
 
             <ChartBarMixed 
@@ -64,7 +64,7 @@ export default async function StatisticPage() {
                     value: String(data?.data?.charts?.categoryChart?.data?.[idx] ?? 0)
                 }))}
                 config={myConfig}
-                className="max-md:col-span-2 w-full h-[400px]"
+                className="max-md:col-span-2 w-full min-h-[400px]"
             />
 
             <ChartPieInteractive 
@@ -74,7 +74,7 @@ export default async function StatisticPage() {
                     value: String(data?.data?.charts?.categoryChart?.data?.[idx] ?? 0)
                 }))}
                 config={myConfig}
-                className="max-md:col-span-2 w-full h-[400px]"
+                className="max-md:col-span-2 w-full min-h-[400px]"
             />
             
             <Card className="flex flex-col max-md:col-span-2">
